e1c2e2c825c25866538de701ca6f516fd48a1077
deevansh <deevanshrana011@gmail.com>
2026-09-17 13:11:07 +0530

    first commit

--- lecture3/Calculater/handler.js
+++ lecture3/Calculater/handler.js
@@ -1,22 +1,24 @@
 const {sumRequestHandler} = require('./sum');
 
-const requesthandler=(req,res)=>{
-    console.log(req.url,req.method);
+const requestHandler = (req, res) => {
+  console.log(req.url, req.method);
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+      <html>
+        <head><title>Practise Set</title></head>
+        <body>
+          <h1>Welcome to Calculator</h1>
+          <a href="/calculator">Go To Calculator</a>
+        </body>  
+      <html>  
+    `);    
+    return res.end();
 
-    if(req.url==='/'){
-        res.setHeader('content-type' , 'text/html');
-        res.write(`<html>
-            <head><title>Practice set</head></title>
-            <body>
-            <h1>welcome to the calculator</h1>
-            <a href="/calculator">go to calculator</a>
-            </body>
-            </html>`);
-            return res.end();
-    }else if(req.url.toLowerCase()==="/calculator"){
-        res.setHeader('content-type' , 'text/html');
-        res.write(`
-        <html>
+  } else if (req.url.toLowerCase() === "/calculator") {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`
+      <html>
         <head><title>Practise Set</title></head>
         <body>
           <h1>Here is the Calculator</h1>
@@ -26,13 +28,26 @@ const requesthandler=(req,res)=>{
             <input type="submit" value="Sum">
           </form>
         </body>  
-        <html> 
-     `);
-     return res.end();
-    }else if (req.url.toLowerCase() === "/calculate-result" &&
+      <html>  
+    `);    
+    return res.end();
+  
+  } else if (req.url.toLowerCase() === "/calculate-result" &&
       req.method === 'POST') {
     return sumRequestHandler(req, res);
-    }
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`
+    <html>
+      <head><title>Practise Set</title></head>
+      <body>
+        <h1>404 Page Does not Exist</h1>
+        <a href="/">Go To Home</a>
+      </body>  
+    <html>  
+  `);    
+  return res.end();
 }
 
 exports.requestHandler = requestHandler;
